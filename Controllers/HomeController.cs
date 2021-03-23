27ab66f5aab38c6d20fd72f6de8e6b093b55f5c5
Anamika <anamika.sawhney22@gmail.com>
2021-03-23 15:29:56 +0530

    Logging Demo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using LoggingNetCoreDemo.Models;
+
+namespace LoggingNetCoreDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            try
+            {
+                int x = 10;
+                int y = 0;
+                int res = x / y;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation("Info");
+                _logger.LogWarning("Warning");
+              
+                _logger.LogCritical(ex.Message);
+                _logger.LogDebug("This is a message");
+            }
+                return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
